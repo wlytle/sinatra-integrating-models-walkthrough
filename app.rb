c516f358afc63b7993f7873554c8c32416a2f6e2
wlytle <wlytle89@gmail.com>
2020-10-26 10:32:17 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
-require_relative 'config/environment'
-require_relative 'models/text_analyzer.rb'
+require_relative "config/environment"
+require_relative "models/text_analyzer.rb"
 
 class App < Sinatra::Base
-  get '/' do
+  get "/" do
     erb :index
   end
 
-  post '/' do
+  post "/" do
     text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(text_from_user)
 
     erb :results
   end
